e35f9f54e2d21ac2ca6a261b6ce0f924aacd24e2
David Vogel <david@truselo.com>
2025-03-03 01:59:48 -0800

    guildhall changes

--- src/GuildHall.tsx
+++ src/GuildHall.tsx
@@ -1,17 +1,21 @@
-let hallLevelNames = [
-    "a dingy shack",
-    "a barn",
-    "a small building",
-    "a medium building",
-    "a large building",
-    "a mansion"
-]
+import { useState } from 'react';
+import data from './data/guildhall.json'
 
 function GuildHall() {
-    let currentHallName = hallLevelNames[0];
+    const [hallLevel, setHallLevel] = useState(0)
+    let currentHallName = data[hallLevel].name;
+
+    function Upgrade() {
+        setHallLevel(hallLevel + 1);
+    }
+
     return (<>
         <div className="text-2xl p-6 border border-gray-600 bg-gray-800 rounded-md gap-x-4">
-            Your current guildhall is: {currentHallName}
+            <p>Your current guildhall is: {currentHallName}</p>
+            {
+                hallLevel + 1 < data.length &&
+                <button onClick={Upgrade}>Upgrade: {data[hallLevel + 1].cost} gold</button>
+            }
         </div>
     </>)
 }
