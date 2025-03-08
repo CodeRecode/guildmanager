f2413e4f2908dcc3a81c435a26387cfce06d3532
David Vogel <david@truselo.com>
2025-03-07 22:30:54 -0800

    Resource bar progress

--- src/character/Character.tsx
+++ src/character/Character.tsx
@@ -8,22 +8,27 @@ export function Character() {
     return (
         <Card>
             <div className="text-center font-bold">Character name</div>
-            <div className="grid grid-cols-3 border border-black rounded-md">
+            <div className="mt-2">
+                <div className="text-center bg-red-400 border rounded-t-sm">{character.health.current + "/" + character.health.max}</div>
+                <div className="text-center bg-green-400 border-x">{character.health.current + "/" + character.health.max}</div>
+                <div className="text-center bg-blue-400 border rounded-b-sm">{character.health.current + "/" + character.health.max}</div>
+            </div>
+            <div className="grid grid-cols-3 border border-black rounded-md mt-4">
                 <div className="border-r">
-                    <Stat name="STR" className="border-b bg-red-400 rounded-tl-md" value={character.str}/>
-                    <Stat name="CON" className=" bg-red-300 rounded-bl-md" value={character.con}/>
+                    <Stat name="STR" className="border-b bg-red-400 rounded-tl-md" value={character.str} />
+                    <Stat name="CON" className=" bg-red-300 rounded-bl-md" value={character.con} />
                 </div>
                 <div className="border-r">
-                    <Stat name="AGI" className="border-b bg-green-400" value={character.agi}/>
-                    <Stat name="REF" className="bg-green-300" value={character.ref}/>
+                    <Stat name="AGI" className="border-b bg-green-400" value={character.agi} />
+                    <Stat name="REF" className="bg-green-300" value={character.ref} />
                 </div>
                 <div>
-                    <Stat name="INT" className="border-b bg-blue-400 rounded-tr-md" value={character.int}/>
-                    <Stat name="WIL" className="bg-blue-300 rounded-br-md" value={character.wil}/>
+                    <Stat name="INT" className="border-b bg-blue-400 rounded-tr-md" value={character.int} />
+                    <Stat name="WIL" className="bg-blue-300 rounded-br-md" value={character.wil} />
                 </div>
             </div>
-            <div className="text-center">
-                Skill Placeholder
+            <div className="text-center font-bold mt-4">
+                Skills
             </div>
         </Card>
     );
--- src/character/CharacterState.ts
+++ src/character/CharacterState.ts
@@ -1,6 +1,20 @@
 import { create } from "zustand";
 
+export class ResourceState {
+    constructor(current: number, max: number) {
+        this.current = current;
+        this.max = max;
+    }
+    
+    current: number = 10;
+    max: number = 10;
+}
+
 export class CharacterState {
+    health: ResourceState = new ResourceState(10, 10);
+    stamina: ResourceState = new ResourceState(10, 10);
+    mana: ResourceState = new ResourceState(10, 10);
+
     str: number = 10;
     con: number = 10;
     agi: number = 10;
